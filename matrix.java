24dede183e8682b79a991135307e217a3535fd45
Kshitiz-Jain <kshitiz16051@iiitd.ac.in>
2017-10-08 09:27:41 +0530

    Linked_Matrix

--- matrix.java
+++ matrix.java
@@ -1,8 +1,6 @@
 package v1.oo;
-
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Matrix implements Serializable {
@@ -36,11 +34,53 @@ public class Matrix implements Serializable {
 		board[m-1][0]=new Cell(2);
 		board[m-1][n-1]=new Cell(2);
 		
+		for(int i=1;i<m-1;i++){
+			for(int j=1;j<n-1;j++){
+				board[i][j].setLink1(board[i][j-1]);
+				board[i][j].setLink2(board[i+1][j]);
+				board[i][j].setLink3(board[i][j+1]);
+				board[i][j].setLink4(board[i-1][j]);
+			}
+		}
+		
+		for(int i=1;i<m-1;i++){
+			board[i][0].setLink1(board[i+1][0]);
+			board[i][0].setLink2(board[i][1]);
+			board[i][0].setLink3(board[i-1][0]);
+			
+			board[i][n-1].setLink1(board[i-1][n-1]);
+			board[i][n-1].setLink1(board[i+1][n-1]);
+			board[i][n-1].setLink1(board[i][n-2]);
+		}
+		
+		for(int i=0;i<n-1;i++){
+			board[0][i].setLink1(board[0][i-1]);
+			board[0][i].setLink2(board[1][i]);
+			board[0][i].setLink3(board[0][i+1]);
+			
+			board[m-1][i].setLink1(board[m-1][i-1]);
+			board[m-1][i].setLink2(board[m-1][i+1]);
+			board[m-1][i].setLink3(board[m-1][i]);
+		}
+		
+		board[0][0].setLink1(board[1][0]);
+		board[0][0].setLink2(board[0][1]);
+		
+		board[m-1][0].setLink1(board[m-1][1]);
+		board[m-1][0].setLink2(board[m-2][0]);
+		
+		board[0][n-1].setLink1(board[0][n-2]);
+		board[0][n-1].setLink2(board[1][n-1]);
+		
+		board[m-1][n-1].setLink1(board[m-1][n-2]);
+		board[m-1][n-1].setLink2(board[m-2][n-1]);
+		
 	}
 	public int getCounter() {
 		return counter;
 	}
 	public static void main(String[] args) throws IOException{
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		int m=scan.nextInt();
 		int n=scan.nextInt();
